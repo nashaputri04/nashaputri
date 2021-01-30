3a5829ffb8a3adca862a8c6ed7118332ca575a0b
nashaputri04 <nashacacaa@gmail.com>
2021-01-30 09:40:38 +0700

    Add New Files

---
+++ Nasha/application/views/v_input.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title>
+</head>
+<body>
+	<center>
+		<h1>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</h1>
+		<h3>Tambah data baru</h3>
+	</center>
+	<form action="<?php echo base_url(). 'crud/tambah_aksi'; ?>" method="post">
+		<table style="margin:20px auto;">
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td>Pekerjaan</td>
+				<td><input type="text" name="pekerjaan"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Tambah"></td>
+			</tr>
+		</table>
+	</form>	
+</body>
+</html>
+
+
+<?php 
+
+
+class Crud extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+		$this->load->helper('url');
+
+	}
+
+	function index(){
+		$data['user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
+	}
+
+	function tambah(){
+		$this->load->view('v_input');
+	}
+
+	function tambah_aksi(){
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+			);
+		$this->m_data->input_data($data,'user');
+		redirect('crud/index');
+	}
+
+}
